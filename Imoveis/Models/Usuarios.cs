076987e225bf2965428fa39f4a3a79c788459821
Ryann Augusto <ryann.cruz97@gmail.com>
2022-04-24 22:20:50 -0300

    Adicionar arquivos de projeto.

---
+++ Imoveis/Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Imoveis.Models
+{
+    [Table(name:"Usuario")]
+    public class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(45)] [Required]
+        public string Name { get; set; }
+
+        public DateTime Data_Nascimento { get; set; }
+
+        [NotMapped]
+        public DateTime Idade { get; set; }
+    }
+}
---
+++ Imoveis/Models/_DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Imoveis.Models
+{
+    public class _DbContext: DbContext
+    {
+        public _DbContext(DbContextOptions<_DbContext> options) : base(options)
+            { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
