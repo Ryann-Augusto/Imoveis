bb17c3aa0cf33a26bc2c81930b53bbdf75889b74
Ryann Barbosa <fiscobrasil2018@gmail.com>
2022-04-29 17:47:45 -0300

    AddClasseQueChamaOsImoveis

---
+++ Imoveis/Migrations/20220429195536_AddMetodoNivel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Imoveis.Migrations
+{
+    public partial class AddMetodoNivel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "nivel",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "nivel",
+                table: "Usuario");
+        }
+    }
+}
---
+++ Imoveis/camada-Dal/Imovel.cs
@@ -0,0 +1,37 @@
+﻿using MySqlConnector;
+using System.Data;
+
+namespace Imoveis.camada_Dal
+{
+    public class Imovel
+    {
+        private string MySqlConn()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var stringConexao = configuration.GetConnectionString("DefaultConnection");
+
+            return stringConexao;
+        }
+
+        public DataTable ObterImoveis()
+        {
+            string queryString = "SELECT ImovelId, imoveldsc, imovelvlr, imovelnumQrt, imovelnumvag, imovelrua, imovelbro, imovelcdd, imoveluf, imovelcep, usu.nome FROM imoveis.imovel INNER JOIN usuario usu ON usuarioid = usu.id; ";
+            using (MySqlConnection connection = new MySqlConnection(MySqlConn()))
+            {
+                MySqlCommand command = new MySqlCommand(queryString, connection);
+                command.Connection.Open();
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter();
+                adapter.SelectCommand = command;
+
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+
+                return table;
+            }
+        }
+    }
+}
