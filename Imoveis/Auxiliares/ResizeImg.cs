7c993fcebae741f45dea7788fd3db7de1e32f2a2
Ryann Barbosa <fiscobrasil2018@gmail.com>
2022-07-05 17:16:14 -0300

    Ajustando dimencoes das imagens

--- Imoveis/Auxiliares/ResizeImg.cs
+++ Imoveis/Auxiliares/ResizeImg.cs
@@ -1,6 +1,4 @@
-﻿using Imoveis.Models;
-using System;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
@@ -28,8 +26,8 @@ namespace Imoveis.Auxiliares
             }
             else
             {
-                int width = 900;
-                int height = 2000;
+                int width = 1200;
+                int height = 1200;
                 var newImage = new Bitmap(width, height);
                 using (Graphics g = Graphics.FromImage(newImage))
                 {
@@ -39,39 +37,39 @@ namespace Imoveis.Auxiliares
             }
             return ms;
         }
-        public static MemoryStream TesteResizeImage(IFormFile imagemCarregada)
-        {
-            Image image = Image.FromStream(imagemCarregada.OpenReadStream(), true, true);
+        //public static MemoryStream TesteResizeImage(IFormFile imagemCarregada)
+        //{
+        //    Image image = Image.FromStream(imagemCarregada.OpenReadStream(), true, true);
 
-            MemoryStream ms = new MemoryStream();
+        //    MemoryStream ms = new MemoryStream();
 
-            if (image.Width > image.Height)
-            {
-                int width = 1920;
-                int height = 1080;
-                var newImage = new Bitmap(width, height);
-                using (Graphics g = Graphics.FromImage(newImage))
-                {
-                    g.DrawImage(newImage, 0, 0, width, height);
-                    newImage.Save(ms, ImageFormat.Jpeg);
-                }
-            }
-            else
-            {
-                int width = 900;
-                int height = 2000;
+        //    if (image.Width > image.Height)
+        //    {
+        //        int width = 1920;
+        //        int height = 1080;
+        //        var newImage = new Bitmap(width, height);
+        //        using (Graphics g = Graphics.FromImage(newImage))
+        //        {
+        //            g.DrawImage(newImage, 0, 0, width, height);
+        //            newImage.Save(ms, ImageFormat.Jpeg);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        int width = 900;
+        //        int height = 2000;
 
-                float y = (image.Height - image.Width) / 2.0F;
-                var retanguloCorte = new Rectangle(0, (int)y, image.Width, image.Width);
+        //        float y = (image.Height - image.Width) / 2.0F;
+        //        var retanguloCorte = new Rectangle(0, (int)y, image.Width, image.Width);
 
-                GraphicsUnit units = GraphicsUnit.Pixel;
-                using (Graphics g = Graphics.FromImage(image))
-                {
-                    g.DrawImage(image, width, height, retanguloCorte, units);
-                    image.Save(ms, ImageFormat.Jpeg);
-                }
-            }
-            return ms;
-        }
+        //        GraphicsUnit units = GraphicsUnit.Pixel;
+        //        using (Graphics g = Graphics.FromImage(image))
+        //        {
+        //            g.DrawImage(image, width, height, retanguloCorte, units);
+        //            image.Save(ms, ImageFormat.Jpeg);
+        //        }
+        //    }
+        //    return ms;
+        //}
     }
 }
--- Imoveis/Controllers/ImoveisController.cs
+++ Imoveis/Controllers/ImoveisController.cs
@@ -209,7 +209,7 @@ namespace Imoveis.Controllers
 
                         if (imagemCarregada != null)
                         {
-                            var img = Auxiliares.ResizeImg.TesteResizeImage(imagemCarregada);
+                            var img = Auxiliares.ResizeImg.ResizeImage(imagemCarregada);
 
                             MdImagens mdImagens = new MdImagens()
                             {
